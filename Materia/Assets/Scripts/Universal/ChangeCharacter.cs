b70f119523195d7ebd3d81881837431cff3cb6fd
MChen7209 <YJChen2922@outlook.com>
2014-11-13 13:57:30 -0600

    Material Folder
    Committing whole material folder

---
+++ Materia/Assets/Scripts/Universal/ChangeCharacter.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ChangeCharacter : MonoBehaviour 
+{
+	private UnifiedSuperClass god;
+	
+	private int currentCharacter = 1;
+
+	private List<GameObject> characters;
+//	private GameObject wizard;
+//	private GameObject warrior;
+//	private GameObject archer;
+	
+	private GameObject current;
+	private Animator currentAnim;
+	private Transform lastSafeLocation;
+
+	private CameraFollow camera;
+
+	void Awake()
+	{
+		god = GameObject.FindGameObjectWithTag ("God").GetComponent<UnifiedSuperClass> ();
+
+		characters = god.getCharacters();
+		camera = GetComponent<CameraFollow>();
+
+		current = characters[1];
+
+		characters[1].SetActive(false);
+		characters[2].SetActive(false);
+
+//		wizard = god
+//		warrior = GameObject.FindGameObjectWithTag ("Warrior");
+//		archer = GameObject.FindGameObjectWithTag ("Archer");
+//
+//		current = GameObject.FindGameObjectWithTag ("Wizard");
+//		camera = GetComponent<CameraFollow>();
+//
+//		archer.SetActive (false);
+//		warrior.SetActive (false);
+	}
+
+	void Update()
+	{
+//		if (Input.GetKeyDown (KeyCode.F1) && currentCharacter != 1)
+//		{
+//			wizard.transform.position = current.transform.position;
+//			current.SetActive (false);
+//			current = wizard;
+//
+//			wizard.SetActive (true);
+//			camera.SwitchPlayer("Wizard");
+//			currentCharacter = 1;
+//		}
+//		if (Input.GetKeyDown (KeyCode.F2) && currentCharacter != 2)
+//		{
+//			warrior.transform.position = current.transform.position;
+//			current.SetActive (false);
+//			current = warrior;
+//
+//			warrior.SetActive (true);
+//			camera.SwitchPlayer("Warrior");
+//			currentCharacter = 2;
+//		}
+//		if (Input.GetKeyDown (KeyCode.F3) && currentCharacter != 3)
+//		{
+//			archer.transform.position = current.transform.position;
+//			current.SetActive (false);
+//			current = archer;
+//
+//			archer.SetActive (true);
+//			camera.SwitchPlayer ("Archer");
+//			currentCharacter = 3;
+//		}
+		currentAnim = current.GetComponent<Animator>();
+
+		if(currentAnim.GetBool("Grounded"))
+			lastSafeLocation = transform;
+
+		if (Input.GetKeyDown (KeyCode.F1) && currentCharacter != 1 && god.isAlive (1))
+		{
+			characters[0].transform.position = current.transform.position;
+			current.SetActive( false);
+			current = characters[0];
+
+			current.SetActive(true);
+			camera.SwitchPlayer(current.tag);
+			currentCharacter = 1;
+		}
+		if (Input.GetKeyDown (KeyCode.F2) && currentCharacter != 2 && god.isAlive (2))
+		{
+			characters[1].transform.position = current.transform.position;
+			current.SetActive( false);
+			current = characters[1];
+			
+			current.SetActive(true);
+			camera.SwitchPlayer(current.tag);
+			currentCharacter = 2;
+		}
+		if (Input.GetKeyDown (KeyCode.F3) && currentCharacter != 3 && god.isAlive (3))
+		{
+			characters[2].transform.position = current.transform.position;
+			current.SetActive( false);
+			current = characters[2];
+			
+			current.SetActive(true);
+			camera.SwitchPlayer(current.tag);
+			currentCharacter = 3;
+		}
+	}
+
+	public void setWhosAlive()
+	{
+		//Lock whos not alive.
+
+	}
+
+	public void changeCharacterCauseDeath(int whoDied, string whatPosition)
+	{
+		if(--whoDied < 0)
+			whoDied = 2;
+
+		if(god.isAlive (whoDied))
+		{
+			characters[whoDied].transform.position = lastSafeLocation.position;
+			current.SetActive(false);
+			current = characters[whoDied];
+			
+			current.SetActive(true);
+			camera.SwitchPlayer(current.tag);
+			currentCharacter = whoDied;
+		}
+	}
+}
---
+++ Materia/Assets/Scripts/Universal/UnifiedSuperClass.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UnifiedSuperClass : MonoBehaviour 
+{
+	//Administration
+	//GameObject[] whosAlive;
+
+	//Status Effects
+	public GameObject frozenThrone;
+
+	//Characters
+	List<GameObject> characters;
+	GameObject wizard;
+	GameObject warrior;
+	GameObject archer;
+
+	//Weapons
+	GameObject wizardWeapon;
+	GameObject warriorWeapon;
+	GameObject archerWeapon;
+
+	//Skills
+	FireballScript fireBallScript;
+
+	LightningNode lightningNodeScript;
+	LightningBolt lightingBoltScript;
+	LightningStrike lightningStrikeScript;
+
+	IceBallScript iceBallScript;
+	IceBlawkOpaque iceBlawkOpaqueScript;
+	IceRadiusLimit iceRadiusLimitScript;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		levelControl (); 
+		characters = new List<GameObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void levelControl(string[] characters, int[] hp/*, scene information*/)
+	{
+		Debug.Log ("Not yet implemented");
+	}
+
+	public void levelControl()
+	{
+		//Maybe use hashmap
+		//Separate by Character1, Character2, Character3
+		//Setup a screen in the menu called, setup team
+		//Can do this for weapons and spells as well.
+		wizard = GameObject.FindGameObjectWithTag ("Wizard");
+		warrior = GameObject.FindGameObjectWithTag ("Warrior");
+		archer = GameObject.FindGameObjectWithTag ("Archer");
+
+		characters.Add (wizard);
+		characters.Add (warrior);
+		characters.Add (archer);
+	}
+
+	public List<GameObject> getCharacters()
+	{
+		return characters;
+	}
+
+	public GameObject getCharacter(string character)
+	{
+		if(character.CompareTo("Wizard"))
+			return wizard;
+		if(character.CompareTo("Archer"))
+			return archer;
+		if(character.CompareTo("Warrior"))
+			return warrior;
+	}
+
+	public GameObject getCharacter(int character)
+	{
+		return characters[character];
+	}
+
+	public void setAlive(string character, bool alive)
+	{
+		//set alive the character in string character
+		//If it is set to all, resurrect all characters that are not alive.
+		GameObject tempCharacterIndex = characters.IndexOf(character);
+		if(alive)
+		{
+			tempCharacterIndex.GetComponentInChildren<PlayerHealth>().setAlive(true);
+			tempCharacterIndex.GetComponentInChildren<PlayerHealth>().setHP ("Full");
+		}
+		else if (!alive)
+		{
+			tempCharacterIndex.GetComponentInChildren<PlayerHealth>().setAlive(false);
+			tempCharacterIndex.GetComponentInChildren<PlayerHealth>().setHP ("None");
+		}
+	}
+
+	public void setAllCharacterAlive(string command)
+	{
+		//set alive the character in string character
+		//If it is set to all, resurrect all characters that are not alive.
+		if(command.CompareTo("AllFull"))
+		{
+			foreach( GameObject element in characters)
+			{
+				element.GetComponentInChildren<PlayerHealth>().setAlive(true);
+				element.GetComponentInChildren<PlayerHealth>().setHP ("Full");
+			}
+		}
+		else if (command.CompareTo("AllNone"))
+		{
+			foreach( GameObject element in characters)
+			{
+				element.GetComponentInChildren<PlayerHealth>().setAlive(false);
+				element.GetComponentInChildren<PlayerHealth>().setHP ("None");
+			}
+		}
+	}
+
+	public string getAlive()
+	{
+		string currentLiving = "";
+
+		foreach(GameObject element in characters)
+		{
+			if (element.GetComponentInChildren<PlayerHealth> ().isAlive ())
+				string.Concat (currentLiving, element.tag + " ");
+		}
+		return currentLiving;
+	}
+
+	public bool isAlive(int characterSlot)
+	{
+		return characters[characterSlot].GetComponentInChildren<PlayerHealth>().isAlive();
+	}
+
+	public void setCheckPoint()
+	{
+	}
+
+	public Transform getCheckPoint()
+	{
+	}
+}
